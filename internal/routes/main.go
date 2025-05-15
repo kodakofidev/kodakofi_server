82f7977e62ea06da0d538f989c9ee9cc20d50b24
redha28 <redhadefinto28@gmail.com>
2025-05-15 21:46:00 +0700

    feat(routes): implement InitRouter function to initialize the Gin router with authentication

---
+++ internal/routes/main.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func InitRouter(db *pgxpool.Pool) *gin.Engine {
+	router := gin.Default()
+
+	auth(router, db)
+	return router
+}
