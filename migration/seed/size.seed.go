9a2d02caa104f7fbc7828b82e83608e92d6fb271
rrhmn <raissarahmani18@gmail.com>
2025-05-18 21:29:59 +0700

    fix(productdetail): add recomendation product and fix some missing feature

---
+++ migration/seed/size.seed.go
@@ -0,0 +1,26 @@
+package seed
+
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func SeedSize(ctx context.Context, db *pgxpool.Pool) error {
+	query := `
+	INSERT INTO sizes (size, added_price)
+	VALUES
+	('Regular', 0),
+	('Medium', 3000),
+	('Large', 5000)
+	`
+	_, err := db.Exec(ctx, query)
+	if err != nil {
+		log.Printf("Failed to seed size: %v", err)
+		return err
+	}
+
+	log.Println("Seeded size successfully.")
+	return nil
+}
