3b22ab8a8f0390d4ad720f62f510820442dd98fe
Irsyad Ridho <irsyadridho.r@gmail.com>
2025-05-19 16:19:00 +0700

    feat(seed): update seed & seed.main.go

---
+++ migration/seed/product_discounts.seed.go
@@ -0,0 +1,32 @@
+package seed
+
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func SeedProductDiscount(ctx context.Context, db *pgxpool.Pool) error {
+	query := `
+		INSERT INTO product_discounts (product_id, discount_id)
+		VALUES  
+			('4e841656-596c-434d-b5bb-1f27f5d7418c', 1),
+			('5740f5fe-5178-4933-9e7b-63cab72fd79a', 1),
+			('e6622f29-2a8b-4110-9a82-8616eed29570', 1),  
+			('7af264d9-fa31-45a4-8948-b2db4c267fd6', 2),
+			('f866f4f6-f89c-4395-90ca-241dfb52951c', 2),
+			('287ec09d-928c-4562-9f29-86ad95dce6f6', 2)
+		ON CONFLICT (product_id, discount_id) DO NOTHING;
+  
+	`
+
+	_, err := db.Exec(ctx, query)
+	if err != nil {
+		log.Printf("Failed to seed product_discounts: %v", err)
+		return err
+	}
+
+	log.Println("Seeded product_discounts successfully.")
+	return nil
+}
--- migration/seed/user.seed.go
+++ migration/seed/user.seed.go
@@ -9,11 +9,11 @@ import (
 
 func SeedUsers(ctx context.Context, db *pgxpool.Pool) error {
 	query := `
-		INSERT INTO public.users (id,email,"password","role",is_verified,created_at,updated_at) VALUES
-			('8a60a062-c0a8-4034-b2f1-a832b9adb435'::uuid,'i08qo@dcpa.net','$argon2id$v=19$m=65536,t=3,p=2$Nw3mnvc4JCgzbYfctP5WeA$CQJ2RCFXV5SRtQCzAwP3G7wSPkUCAOYrcOR25Ubk7sU','user',true,'2025-05-18 20:42:23.05615+07',NULL),
-			('171b67f9-a3c2-4115-b75c-1a7816ca1cd2'::uuid,'sdgi6@dcpa.net','$argon2id$v=19$m=65536,t=3,p=2$jEpY2BHM0zseUt//j13fNQ$1ORFua0u/0YCfj47Olmuf+t27DEcWPqGB0KvlGCM+bo','user',true,'2025-05-18 20:47:26.90677+07',NULL),
-			('c59b7e07-cbb6-4647-858a-406c3c4b4bd2'::uuid,'xobeba1095@magpit.com','$argon2id$v=19$m=65536,t=3,p=2$MH9T37oskorZGmHyuZ5Uyg$yyCmvc+buc5croTd4TmxLtqjaOd1D3LvXWOnyMl8AQ0','admin',true,'2025-05-18 18:28:38.854598+07',NULL),	 
-			('d66619fb-d37a-47d4-b2e3-841691fbd1a1'::uuid,'rifef84546@neuraxo.com','$argon2id$v=19$m=65536,t=3,p=2$VTNHlDL3mdn/Ll6mYRnKrQ$n7Ic1cr7LKZeYEnBRLrN08iuLvoOaUnQ9CuH5T6KOpw','user',true,'2025-05-19 03:49:52.892776+07',NULL)
+		INSERT INTO public.users (id, email, "password", "role", is_verified, created_at, updated_at) VALUES
+			('c59b7e07-cbb6-4647-858a-406c3c4b4bd2'::uuid, 'xobeba1095@magpit.com', '$argon2id$v=19$m=65536,t=3,p=2$MH9T37oskorZGmHyuZ5Uyg$yyCmvc+buc5croTd4TmxLtqjaOd1D3LvXWOnyMl8AQ0', 'admin', true, '2025-05-18 18:28:38.854598+07', NULL),
+			('171b67f9-a3c2-4115-b75c-1a7816ca1cd2'::uuid, 'sdgi6@dcpa.net', '$argon2id$v=19$m=65536,t=3,p=2$jEpY2BHM0zseUt//j13fNQ$1ORFua0u/0YCfj47Olmuf+t27DEcWPqGB0KvlGCM+bo', 'user', true, '2025-05-18 20:47:26.90677+07', NULL),
+			('0ee41132-5592-4626-8f87-1df70db54e3f'::uuid, 'testuser@example.com', '$argon2id$v=19$m=65536,t=3,p=2$abc123$xyz4567890', 'user', true, '2025-05-19 03:49:14.898142+07', NULL),
+			('8a60a062-c0a8-4034-b2f1-a832b9adb435'::uuid, 'i08qo@dcpa.net', '$argon2id$v=19$m=65536,t=3,p=2$Nw3mnvc4JCgzbYfctP5WeA$CQJ2RCFXV5SRtQCzAwP3G7wSPkUCAOYrcOR25Ubk7sU', 'user', true, '2025-05-18 20:42:23.05615+07', NULL)
 		ON CONFLICT (id) DO NOTHING;
 	`
 
