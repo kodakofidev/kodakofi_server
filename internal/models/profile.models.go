1ba06bd31b364309d69f74aa992e22a727600743
Imanul Aufa <imanulaufa@gmail.com>
2025-05-19 15:09:50 +0700

    fix(profile model): fix profile form password field(add current password and newpassword)

--- internal/models/profile.models.go
+++ internal/models/profile.models.go
@@ -1,21 +1,30 @@
 package models
 
-import "mime/multipart"
+import (
+	"mime/multipart"
+	"time"
+)
 
 type ProfileUser struct {
 	Fullname string `json:"fullname" form:"fullname"`
 	Phone    string `json:"phone" form:"phone"`
 	Address  string `json:"address" form:"address"`
+	// Password string `json:"-" form:"password" binding:"required,min=8"`
 }
 
 type ProfileForm struct {
 	ProfileUser
 	ProfileImage *multipart.FileHeader `form:"profileImage"`
+	// NewPass      string                `json:"-" form:"newPassword" binding:"required,min=8"`
+	CurrentPassword string `json:"currentPassword" form:"currentPassword" binding:"required_with=NewPassword"`
+	NewPassword     string `json:"newPassword" form:"newPassword" binding:"omitempty,min=8,max=72"`
 }
 
 type Profile struct {
 	ProfileUser
-	ProfileImage string `json:"profileImage"`
+	ProfileImage string    `json:"profileImage"`
+	CreatedAt    time.Time `json:"createdAt"`
+	UpdatedAt    time.Time `json:"updatedAt"`
 }
 
 type Profiles []Profile
