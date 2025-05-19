894a21ffba08c85aba38633e389364784fceb389
rrhmn <raissarahmani18@gmail.com>
2025-05-19 23:41:56 +0700

    fix(product): filter query

--- internal/repositories/product.repositories.go
+++ internal/repositories/product.repositories.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"log"
 	"strings"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -17,7 +15,6 @@ type ProductRepoInterface interface {
 	GetAllProducts(c context.Context, params *models.ProductQueryParams) (*models.PaginatedResponse, error)
 	GetDetailProduct(c context.Context, id string) (*models.Product, error)
 	GetRecommendation(c context.Context, limit int) (models.Products, error)
-	AddProduct(c context.Context, newProduct *models.ProductRequest, images []string) error
 }
 
 type RepoProduct struct {
@@ -41,8 +38,11 @@ func (r *RepoProduct) GetAllProducts(c context.Context, params *models.ProductQu
 	argIndex := 1 // Parameter index dimulai dari 1 untuk CTE
 
 	// Filter harga
-	if params.Min > 0 || params.Max > 0 {
+	if params.Min >= 0 || params.Max >= 0 {
 		minPrice := params.Min
+		if minPrice < 0 {
+			minPrice = 0
+		}
 		maxPrice := params.Max
 		if maxPrice <= 0 {
 			maxPrice = 1000000
@@ -122,6 +122,7 @@ func (r *RepoProduct) GetAllProducts(c context.Context, params *models.ProductQu
     JOIN categories c ON c.id = p.category_id
     CROSS JOIN product_stats ps
     GROUP BY p.id, d.id, c.name, ps.total_filtered`
+
 	// Tambahkan ORDER BY
 	switch params.Options {
 	case "oldest":
@@ -326,78 +327,4 @@ func (r *RepoProduct) GetRecommendation(c context.Context, limit int) (models.Pr
 	}
 
 	return products, nil
-}
-func (r *RepoProduct) AddProduct(c context.Context, newProduct *models.ProductRequest, listImage []string) error {
-	tx, err := r.DB.Begin(c)
-	if err != nil {
-		log.Println("[DEBUG 1]", err)
-		return err
-	}
-
-	func() {
-		if err != nil {
-			tx.Rollback(c)
-		}
-	}()
-	query := `insert into products (name, category_id, price,description) values ($1, $2,$3,$4) returning id`
-	var productID models.ProductRequest
-	values := []any{newProduct.Name, newProduct.CategoryID, newProduct.Price, newProduct.Description}
-	log.Println("[DEBUG LENGTH]", len(newProduct.Size))
-	if len(newProduct.Size) == 0 {
-		newProduct.Size = append(newProduct.Size, 4)
-	}
-	err = tx.QueryRow(c, query, values...).Scan(&productID.Id)
-	log.Println("[DEBUG ID]", productID.Id)
-	if err != nil {
-		log.Println("[ERR ]", err)
-	}
-	log.Println("[ID]", productID)
-	querySize := `insert into size_products (product_id, stock,size_id) values`
-	valuesSize := []any{productID.Id, newProduct.Stock}
-	log.Println("DEBUG SIZE", newProduct.Size)
-	for i, size := range newProduct.Size {
-		if i > 0 {
-			querySize += ","
-		}
-		querySize += fmt.Sprintf("($1, $2,$%d)", i+3)
-		valuesSize = append(valuesSize, size)
-	}
-	log.Println("[valuessize]", valuesSize)
-	cmd, err := tx.Exec(c, querySize, valuesSize...)
-	if err != nil {
-		log.Println("[DEBUG 2]", err)
-		return errors.New("add product failed")
-	}
-	row := cmd.RowsAffected()
-	if row == 0 {
-		return errors.New("add product failed ")
-	}
-
-	queryImage := `insert into product_images (product_id, path) values`
-	valuesImage := []any{productID.Id}
-	for i, image := range listImage {
-		if i > 0 {
-			queryImage += ","
-		}
-		queryImage += fmt.Sprintf("($1,$%d)", i+2)
-		valuesImage = append(valuesImage, image)
-	}
-	log.Println("[DEBUG 2]", queryImage)
-
-	cmd, err = tx.Exec(c, queryImage, valuesImage...)
-
-	if err != nil {
-		log.Println("[DEBUG 3]", err)
-
-		return errors.New("add path image failed")
-	}
-	row = cmd.RowsAffected()
-	if row == 0 {
-		return errors.New("add path image failed")
-	}
-	if err := tx.Commit(c); err != nil {
-		return err
-	}
-
-	return nil
 }
