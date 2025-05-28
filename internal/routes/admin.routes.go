37c16c759ebc7098c63952f2659864ee16b255d7
Irsyad Ridho <irsyadridho.r@gmail.com>
2025-05-28 15:14:15 +0700

    feat(route user): add feat route update user by admin

--- internal/routes/admin.routes.go
+++ internal/routes/admin.routes.go
@@ -20,6 +20,7 @@ func admin(r *gin.RouterGroup, db *pgxpool.Pool, mdw *middlewares.Middleware) {
 	route.GET("data-sales", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersOrder.FetchDataSalesHandler)
 	route.PATCH("status", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersOrder.UpdateOrderStatus)
 	route.GET("users", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersUser.FetchAllUsersHandler)
+	route.PATCH("users/:id", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersUser.PatchUserByAdminHandler)
 	// route.PATCH("user", mdw.VerifyToken, mdw.AccsessGate("admin"), handlers.)
 	// route.POST("user", mdw.VerifyToken, mdw.AccsessGate("admin"), handlers.)
 }
