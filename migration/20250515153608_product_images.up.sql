a5cffeaa1bfec9b873fe5c54ddec812de7cbc09e
Irsyad Ridho <irsyadridho.r@gmail.com>
2025-05-29 17:24:06 +0700

    chore(product images): update default value for image column

--- migration/20250515153608_product_images.up.sql
+++ migration/20250515153608_product_images.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS product_images (
   product_id uuid NOT NULL,
-  path varchar NOT NULL,
+  path varchar NOT NULL DEFAULT '',
   created_at timestamptz DEFAULT now() NOT NULL,
   updated_at timestamptz,
   CONSTRAINT product_images_pk PRIMARY KEY (product_id, path),
