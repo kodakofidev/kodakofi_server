37dd093a1c506464935afe6dcec67e6fcc42ad1f
Irsyad Ridho <irsyadridho.r@gmail.com>
2025-05-30 01:47:01 +0700

    feat(order details admin): add feat route order details by admin

--- internal/routes/admin.routes.go
+++ internal/routes/admin.routes.go
@@ -9,7 +9,7 @@ import (
 )
 
 func admin(r *gin.RouterGroup, db *pgxpool.Pool, mdw *middlewares.Middleware) {
-	route := r.Group("admin")
+	route := r.Group("/admin")
 
 	repoOrder := repositories.NewOrder(db)
 	repoUser := repositories.NewUser(db)
@@ -19,11 +19,13 @@ func admin(r *gin.RouterGroup, db *pgxpool.Pool, mdw *middlewares.Middleware) {
 	handlersUser := handlers.NewUser(repoUser)
 	handlersProduct := handlers.NewProduct(repoProduct)
 
-	route.GET("data-sales", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersOrder.FetchDataSalesHandler)
-	route.PATCH("status", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersOrder.UpdateOrderStatus)
-	route.GET("users", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersUser.FetchAllUsersHandler)
-	route.PATCH("users/:id", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersUser.PatchUserByAdminHandler)
-	// route.PATCH("user", mdw.VerifyToken, mdw.AccsessGate("admin"), handlers.)
-	// route.POST("user", mdw.VerifyToken, mdw.AccsessGate("admin"), handlers.)
-	route.GET("products", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersProduct.FetchAllProductsAdminHandler)
+	route.GET("/data-sales", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersOrder.FetchDataSalesHandler)
+	route.PATCH("/status", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersOrder.UpdateOrderStatus)
+	route.GET("/users", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersUser.FetchAllUsersHandler)
+	// route.GET("/users/:id", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersUser.)
+	route.PATCH("/users/:id", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersUser.PatchUserByAdminHandler)
+	// route.POST("/user", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersUser.)
+	route.GET("/products", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersProduct.FetchAllProductsAdminHandler)
+	// route.GET("/orders", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersOrder.FetchDetailOrderAdminHandler)
+	route.GET("/orders/:transaction_code", mdw.VerifyToken, mdw.AccsessGate("admin"), handlersOrder.FetchDetailOrderAdminHandler)
 }
